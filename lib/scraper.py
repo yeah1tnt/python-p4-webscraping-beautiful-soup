336b149ffd6bfbceef63ebf80cc5772fe1c1203b
Tong Tang <pichip22@hotmail.com>
2023-09-11 10:35:04 -0400

    completed

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,7 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.site-header')[0].contents)
